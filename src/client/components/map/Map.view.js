9a3bf2b96fa2eae92e129db0027a5c0435a7f57f
Thomas Lorincz <tlorincz@ualberta.ca>
2019-05-28 10:18:56 -0600

    Added district geojson layer to map

--- src/client/components/map/Map.view.js
+++ src/client/components/map/Map.view.js
@@ -18,7 +18,31 @@ export default class MapView extends View {
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9',
       center: [-113.323975, 53.631611],
-      zoom: 4,
+      zoom: 7,
+    });
+
+    this.map.on('load', () => {
+      this.map.addLayer({
+        'id': 'districtLayer',
+        'source': {
+          type: 'vector',
+          url: 'mapbox://thomaslorincz.7qoflzqc',
+        },
+        'source-layer': 'district1669-5c4o7a',
+        'type': 'fill',
+        'paint': {
+          'fill-color': 'rgba(0,0,255,0.5)',
+          'fill-outline-color': 'rgba(0,0,255,0.2)',
+        },
+      });
+    });
+
+    this.map.on('mouseenter', 'districtLayer', () => {
+      this.map.getCanvas().style.cursor = 'pointer';
+    });
+
+    this.map.on('mouseleave', 'districtLayer', () => {
+      this.map.getCanvas().style.cursor = '';
     });
   }
 }
