c635fb03c9635bf77bc6ed2198bd764b695897d3
Jingyuanisxzs <jingyuan981123@gmail.com>
2019-05-30 10:38:04 -0600

    added popup to show the number of trips

--- src/client/components/map/Map.view.js
+++ src/client/components/map/Map.view.js
@@ -136,6 +136,18 @@ export default class MapView extends View {
         ],
       },
     });
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+    this.map.on('mouseenter', line[5], (e) =>{
+      popup.setLngLat(e.lngLat)
+          .setHTML('Number of Trips: ' + line[4])
+          .addTo(this.map);
+    });
+    this.map.on('mouseleave', line[5], () =>{
+      popup.remove();
+    });
   }
 
   /**
