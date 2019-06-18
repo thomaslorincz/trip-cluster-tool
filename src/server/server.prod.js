75d69fd22d5c5d3cbf3cdd36e824297e21011ff7
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-18 12:14:31 -0600

    Add ability for server to serve Brotli compressed files if available

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -1,11 +1,13 @@
-import path from 'path';
 import express from 'express';
+import expressStaticGzip from 'express-static-gzip';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.use('/', expressStaticGzip(__dirname, {
+  enableBrotli: true,
+  customCompressions: [],
+  orderPreference: ['br'],
+}));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
