e4dc82e9546520343332be71d55c959ed707092b
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-20 14:13:04 -0600

    Add SSL redirection to server.prod config

--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
+import sslRedirect from 'heroku-ssl-redirect';
 import expressStaticGzip from 'express-static-gzip';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(helmet());
-
+app.use(sslRedirect());
 app.use('/', expressStaticGzip(__dirname, {
   enableBrotli: true,
   customCompressions: [],
