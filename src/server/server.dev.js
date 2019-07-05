491c711cabf824c1717d6771941f203f12b09214
Thomas Lorincz <tlorincz@ualberta.ca>
2019-07-05 15:11:22 -0600

    Add express-static-gzip middleware to server dev config

--- src/server/server.dev.js
+++ src/server/server.dev.js
@@ -5,15 +5,20 @@ import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from '../../webpack.dev.config.js';
+import expressStaticGzip from 'express-static-gzip';
 
 const app = express();
 const port = process.env.PORT || 8080;
 const compiler = webpack(config);
 
 app.use(helmet());
-
 app.use(webpackDevMiddleware(compiler, {publicPath: config.output.publicPath}));
 app.use(webpackHotMiddleware(compiler));
+app.use('/', expressStaticGzip(__dirname, {
+  enableBrotli: true,
+  customCompressions: [],
+  orderPreference: ['br'],
+}));
 
 app.get('/', (req, res, next) => {
   compiler.outputFileSystem.readFile(
