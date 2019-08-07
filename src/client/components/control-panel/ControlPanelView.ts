dbdeecb9203446105e4f8583e6f4e9866e330b18
Thomas Lorincz <tlorincz@ualberta.ca>
2019-08-07 12:31:34 -0600

    Separate selection logic from ControlPanelView

--- src/client/components/control-panel/ControlPanelView.ts
+++ src/client/components/control-panel/ControlPanelView.ts
@@ -3,19 +3,14 @@ import * as EventEmitter from 'eventemitter3';
 
 /** A view that represents a panel with process controls and information. */
 export default class ControlPanelView extends View {
-  private readonly selectedGeography: HTMLElement;
-  private readonly selectedLine: HTMLElement;
   private readonly flowLinesDecrement: HTMLElement;
   private readonly numFlowLines: HTMLElement;
   private readonly flowLinesIncrement: HTMLElement;
-  private readonly boundaryEntries: NodeListOf<HTMLElement>;
+  private readonly geographyEntries: NodeListOf<HTMLElement>;
   private readonly modeEntries: NodeListOf<HTMLElement>;
 
   public constructor(container: Element, emitter: EventEmitter) {
     super(container, emitter);
-
-    this.selectedGeography = document.getElementById('selected-geography');
-    this.selectedLine = document.getElementById('selected-line');
 
     this.flowLinesDecrement = document.getElementById('flow-lines-decrement');
     this.flowLinesDecrement.addEventListener('click', (): void => {
@@ -29,11 +24,11 @@ export default class ControlPanelView extends View {
       this.emitter.emit('incrementClicked');
     });
 
-    this.boundaryEntries = document.querySelectorAll('.boundary-entry');
-    this.boundaryEntries.forEach((entry: HTMLElement): void => {
+    this.geographyEntries = document.querySelectorAll('.geography-entry');
+    this.geographyEntries.forEach((entry: HTMLElement): void => {
       entry.addEventListener('click', (event: Event): void => {
         if (event.target instanceof HTMLElement) {
-          this.emitter.emit('boundaryClicked', event.target.dataset.value);
+          this.emitter.emit('geographyClicked', event.target.dataset.value);
         }
       });
     });
@@ -48,46 +43,21 @@ export default class ControlPanelView extends View {
     });
   }
 
-  public draw(geography: number, lineWeight: number, numFlowLines: number,
-      boundary: string, mode: string): void {
-    if (geography === -1) {
-      this.selectedGeography.innerText = 'Nothing Selected';
-      this.selectedGeography.classList.remove('selected-text');
-    } else {
-      if (boundary === 'district') {
-        this.selectedGeography.innerText = `District ${geography}`;
-      } else {
-        this.selectedGeography.innerText = `Zone ${geography}`;
-      }
-      this.selectedGeography.classList.add('selected-text');
-    }
-
-    if (lineWeight === -1) {
-      this.selectedLine.innerText = '';
-    } else {
-      this.selectedLine.innerText = `Line Trips: ${lineWeight}`;
-    }
-
+  public draw(numFlowLines: number, geographyType: string, mode: string): void {
     this.numFlowLines.innerText = numFlowLines.toString();
 
-    document.querySelectorAll('.content.selected').forEach((content): void => {
-      const radioButton = content.querySelector('.content-radio-button');
-      radioButton.innerHTML = 'radio_button_unchecked';
-      content.classList.remove('selected');
-    });
-
-    const selectedBoundary = document.getElementById(`boundary-${boundary}`);
-    if (selectedBoundary) {
-      selectedBoundary.querySelector('.content-radio-button').innerHTML
-          = 'radio_button_checked';
-      selectedBoundary.classList.add('selected');
+    const selectedGeography = this.container
+        .querySelector('.geography-entry.selected');
+    if (selectedGeography) {
+      selectedGeography.classList.remove('selected');
     }
+    document.getElementById(`${geographyType}-entry`).classList.add('selected');
 
-    const selectedMode = document.getElementById(`mode-${mode}`);
+    const selectedMode = this.container
+        .querySelector('.mode-entry.selected');
     if (selectedMode) {
-      selectedMode.querySelector('.content-radio-button').innerHTML
-          = 'radio_button_checked';
-      selectedMode.classList.add('selected');
+      selectedMode.classList.remove('selected');
     }
+    document.getElementById(`${mode}-entry`).classList.add('selected');
   }
 }
