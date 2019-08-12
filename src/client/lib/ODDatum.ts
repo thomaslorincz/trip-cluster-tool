93814415f6ae9a19448bdc63c056a3988702686e
Thomas Lorincz <tlorincz@ualberta.ca>
2019-08-12 09:34:58 -0600

    Modify ODDatum to support 3 mode weight fields

--- src/client/lib/ODDatum.ts
+++ src/client/lib/ODDatum.ts
@@ -4,26 +4,28 @@ export default class ODDatum {
   public destZone: number;
   public originDistrict: number;
   public destDistrict: number;
-  public mode: string;
   public originX: number;
   public originY: number;
   public destX: number;
   public destY: number;
-  public weight: number;
+  public auto: number;
+  public transit: number;
+  public active: number;
 
   public constructor(originZone: number, destZone: number,
-      originDistrict: number, destDistrict: number, mode: string,
-      originX: number, originY: number, destX: number, destY: number,
-      weight: number) {
+      originDistrict: number, destDistrict: number, originX: number,
+      originY: number, destX: number, destY: number, autoWeight: number,
+      transitWeight: number, activeWeight: number) {
     this.originZone = originZone;
     this.destZone = destZone;
     this.originDistrict = originDistrict;
     this.destDistrict = destDistrict;
-    this.mode = mode;
     this.originX = originX;
     this.originY = originY;
     this.destX = destX;
     this.destY = destY;
-    this.weight = weight;
+    this.auto = autoWeight;
+    this.transit = transitWeight;
+    this.active = activeWeight;
   }
 }
