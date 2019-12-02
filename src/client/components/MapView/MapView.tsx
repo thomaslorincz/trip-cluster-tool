abb40ad42eea86d60d292aa13c79e1d63f0c423e
Thomas Lorincz <tlorincz@ualberta.ca>
2019-12-02 15:26:08 -0700

    Implement DeckGl with StaticMap layer in MapView component

--- src/client/components/MapView/MapView.tsx
+++ src/client/components/MapView/MapView.tsx
@@ -2,8 +2,36 @@ import * as React from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './MapView.css';
 
+import DeckGL from '@deck.gl/react';
+import { StaticMap } from 'react-map-gl';
+
 export class MapView extends React.Component<{}, {}> {
+  public componentDidMount(): void {
+    document
+      .getElementById('deckgl-wrapper')
+      .addEventListener('contextmenu', event => event.preventDefault());
+  }
+
   public render(): React.ReactNode {
-    return <div className="map" />;
+    return (
+      <DeckGL
+        layers={[]}
+        initialViewState={{
+          longitude: -113.4938,
+          latitude: 53.5461,
+          zoom: 11,
+          pitch: 45,
+          bearing: 0
+        }}
+        controller={true}
+      >
+        <StaticMap
+          reuseMaps
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+          preventStyleDiffing={true}
+          mapboxApiAccessToken={process.env.MAPBOX_TOKEN}
+        />
+      </DeckGL>
+    );
   }
 }
