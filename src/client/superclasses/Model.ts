841b6afd2cacc09077283ad7bc7264e64b7b9f61
Thomas Lorincz <tlorincz@ualberta.ca>
2019-12-02 14:54:39 -0700

    Remove MVP framework codebase

--- src/client/superclasses/Model.ts
+++
@@ -1,9 +0,0 @@
-import * as EventEmitter from 'eventemitter3';
-
-export default class Model {
-  protected readonly emitter: EventEmitter;
-
-  public constructor(emitter: EventEmitter) {
-    this.emitter = emitter;
-  }
-}
